8c043d4c30892a21c8f127dd912c50ac3252d90b
Thomas Palmen <121097637+thmaz@users.noreply.github.com>
2023-11-20 12:46:36 +0100

    Add project files.

---
+++ FridgeAlert/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FridgeAlert
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FridgeAlert/FridgeList.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FridgeAlert
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FridgeList : ContentPage
+    {
+        public FridgeList()
+        {
+            InitializeComponent();
+        }
+    }
+}
