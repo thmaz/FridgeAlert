e3150c2cf78bc73fc5f22c3ef12c3e8a65980ef5
Thomas Palmen <121097637+thmaz@users.noreply.github.com>
2023-12-07 16:42:40 +0100

    Updated layout

---
+++ FridgeAlert/Models/Groceries.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FridgeAlert.Models
+{
+    class Groceries
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
