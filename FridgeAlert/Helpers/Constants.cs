04a9faa9580eda7fc6a00a7aaad0dee75f617ba6
Thomas Palmen <121097637+thmaz@users.noreply.github.com>
2023-12-17 20:00:14 +0100

    Added class constants for beer

---
+++ FridgeAlert/Helpers/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FridgeAlert.Helpers;
+
+public class Constants
+{
+    public const string RANDOM_BEER = "https://api.punkapi.com/v2/beers/random/";
+}
