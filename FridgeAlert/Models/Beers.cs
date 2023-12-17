51e7c94efeb0bdb63b872519806db7633decfc32
Thomas Palmen <121097637+thmaz@users.noreply.github.com>
2023-12-17 20:00:28 +0100

    made class for beers api

---
+++ FridgeAlert/Models/Beers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FridgeAlert.Helpers;
+
+namespace FridgeAlert.Models;
+
+public class Beers
+{
+    public static string GenerateUrlRandom()
+    {
+        return Constants.RANDOM_BEER;
+    }
+}
